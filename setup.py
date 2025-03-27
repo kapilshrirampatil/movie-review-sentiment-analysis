08506e4f77da22f0a55b62218fc4c5e355ca8755
kapil patil <kapilshrirampatil@gmail.com>
2025-03-27 18:52:39 +0530

    cookiecutter template is created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an end to end solution where on the basis of given review ML model will classify whether the review is positive or negative',
+    author='Kapil Patil',
+    license='',
+)
